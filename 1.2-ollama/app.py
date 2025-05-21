903ed14f4267c8ad147d38fc01c5c51ebad987de
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-05-21 16:43:44 +0200

    Create the app structure

---
+++ 1.2-ollama/app.py
@@ -0,0 +1,14 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_community.llms import Ollama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+## Langsmith Tracking
+os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['LANGCHAIN_PROJECT'] = os.getenv('LANGCHAIN_PROJECT')
